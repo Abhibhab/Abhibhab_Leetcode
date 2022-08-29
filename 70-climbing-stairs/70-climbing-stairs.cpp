b9caa508e386c1ae6c1c31d2e26f5a3b1eae92b1
Abhibhab Mitra Thakur <70086453+Abhibhab@users.noreply.github.com>
2022-08-29 19:38:49 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (40.95%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,33 @@
+
+class Solution {
+public:
+    int helper(int n,vector<int>&ans){
+        if(n<=0) return 1;
+        if(n==1){
+            return 1;
+        }
+        if(ans[n]!=-1){
+            return ans[n];
+        }
+        int small=helper(n-1,ans);
+        int small2=helper(n-2,ans);
+        // int small3=climbStairs(n-3);
+       ans[n]=small+small2;
+            return ans[n];
+        
+        
+    }
+    int climbStairs(int n) {
+        vector<int>vec(50);
+        for(int i=0;i<50;i++){
+            vec[i]=-1;
+        }
+        return helper(n,vec);
+
+        
+
+        
+        
+        
+    }
+};
