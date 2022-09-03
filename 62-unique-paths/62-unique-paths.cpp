ff29ca6452125da1884bffc2e91ba7674078cb9b
Abhibhab Mitra Thakur <70086453+Abhibhab@users.noreply.github.com>
2022-09-03 16:52:33 +0530

    Time: 4 ms (28.81%), Space: 6.5 MB (46.49%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -13,10 +13,29 @@ public:
         
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>vec(m,vector<int>(n,-1));
+        vector<vector<int>>vec(m,vector<int>(n,0));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) vec[i][j]=1;
+                else{
+                    int left=0,up=0;
+                    if(j>0){
+                        left=vec[i][j-1];
+                        
+                    }
+                    if(i>0){
+                        up=vec[i-1][j];
+                    }
+                    vec[i][j]=left+up;
+                        
+                    
+                }
+            }
+        }
+        return vec[m-1][n-1];
         
         
-        return helper(m-1,n-1,m,n,vec);
+        // return helper(m-1,n-1,m,n,vec);
         
     }
 };
