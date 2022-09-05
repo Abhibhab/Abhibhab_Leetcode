e23e5b967effa74cda4d6ac98fd3f6f071d6749a
Abhibhab Mitra Thakur <70086453+Abhibhab@users.noreply.github.com>
2022-09-05 10:16:16 +0530

    Time: 167 ms (40.08%), Space: 45.1 MB (31.18%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,13 +1,36 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>>vec(coins.size(),vector<int>(amount+1,-1));
-        int ans= helper(coins.size()-1,coins,amount,vec);
-        if(ans>=1e9){
-            return -1;
-        }else{
-            return ans;
+//         vector<vector<int>>vec(coins.size(),vector<int>(amount+1,-1));
+//         int ans= helper(coins.size()-1,coins,amount,vec);
+//         if(ans>=1e9){
+//             return -1;
+//         }else{
+//             return ans;
+//         }
+        
+        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,1e9));
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0)dp[0][i]=i/coins[0];
         }
+        for(int index=1;index<coins.size();index++){
+            for(int amo=0;amo<=amount;amo++){
+         int take=INT_MAX;
+        if(coins[index] <= amo) 
+            take = 1 + dp[index][amo-coins[index]];
+        int notake=dp[index-1][amo];
+        
+        dp[index][amo]=min(notake,take);
+        
+        }
+        }
+            
+        int ans=dp[coins.size()-1][amount];
+        if(ans>=1e9)return -1;
+        else return ans;
+    
+        
+        
         
     }
     int helper(int index,vector<int>&coins,int amount,vector<vector<int>>&vec){
