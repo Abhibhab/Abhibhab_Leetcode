5d8107ab6f0c468c69271ecffdb1f9025ed65f13
Abhibhab Mitra Thakur <70086453+Abhibhab@users.noreply.github.com>
2022-09-05 22:58:31 +0530

    Time: 46 ms (23.70%), Space: 17.8 MB (62.19%) - LeetHub

---
+++ 135-candy/135-candy.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        if(ratings.size()<=1)return 1;
+        vector<int>arr(ratings.size(),1);
+        for(int i=1;i<ratings.size();i++){
+            if(ratings[i]>ratings[i-1])arr[i]=arr[i-1]+1;
+        }
+        for(int i=ratings.size()-1;i>0;i--){
+             if(ratings[i-1]>ratings[i])
+			 arr[i-1]=max(arr[i]+1,arr[i-1]);
+        }
+        // for(auto it:arr){
+        //     cout<<it<<" ";
+        // }
+        // cout<<endl;
+        return accumulate(arr.begin(),arr.end(),0);
+    }
+};
