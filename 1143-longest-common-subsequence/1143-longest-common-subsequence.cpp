69e74e455d216830ac43d096336b835580f7b36f
Abhibhab Mitra Thakur <70086453+Abhibhab@users.noreply.github.com>
2022-09-05 19:56:45 +0530

    Time: 22 ms (77.77%), Space: 13.1 MB (23.86%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -13,14 +13,19 @@ public:
         // vector<vector<int>>dp(sizea+1,vector<int>(sizeb+1,-1));
         // return helper(sizea,sizeb,text1,text2,dp);
         vector<vector<int>>dp(sizea+1,vector<int>(sizeb+1,0));
+        vector<int>prev(sizeb+1,0);
+        vector<int>curr(sizeb+1,0);
+
+        
        for(int i=1;i<=sizea;i++){
            for(int j=1;j<=sizeb;j++){
-        if(text1[i-1]==text2[j-1])dp[i][j]=1 +dp[i-1][j-1];
-       else dp[i][j]= max(dp[i-1][j],dp[i][j-1]);
+            if(text1[i-1]==text2[j-1])curr[j]=1 +prev[j-1];
+            else curr[j]= max(prev[j],curr[j-1]);
                
            }
+           prev=curr;
        }
-        return dp[sizea][sizeb];
+        return prev[sizeb];
 
         
         
