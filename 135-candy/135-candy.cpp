5b16cc37374e8dfbfeb3a7a6177609aa506b57db
Abhibhab Mitra Thakur <70086453+Abhibhab@users.noreply.github.com>
2022-09-05 23:06:28 +0530

    Time: 49 ms (18.99%), Space: 17.7 MB (78.40%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -7,8 +7,10 @@ public:
             if(ratings[i]>ratings[i-1])arr[i]=arr[i-1]+1;
         }
         for(int i=ratings.size()-1;i>0;i--){
-             if(ratings[i-1]>ratings[i])
-			 arr[i-1]=max(arr[i]+1,arr[i-1]);
+             if(ratings[i-1]>ratings[i]){
+                 if(arr[i-1]>arr[i]+1)continue;
+			else arr[i-1]=arr[i]+1;
+             }
         }
         // for(auto it:arr){
         //     cout<<it<<" ";
