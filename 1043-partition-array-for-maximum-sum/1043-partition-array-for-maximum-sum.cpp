217789e729c954ab4d98f31a97d51175ef759f89
Abhibhab Mitra Thakur <70086453+Abhibhab@users.noreply.github.com>
2022-09-10 23:52:13 +0530

    Time: 42 ms (21.55%), Space: 8.6 MB (67.22%) - LeetHub

---
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        vector<int>vec(arr.size()+1,-1);
+        return helper(0,arr,k,vec);
+      
+        
+    }
+    int helper(int ind,vector<int>&arr,int k, vector<int>&vec){
+        int n=arr.size();
+        if(ind==n)return 0;
+        if(vec[ind]!=-1)return vec[ind];
+        int maxi=INT_MIN;
+        int curr=INT_MIN;
+        int length=0;
+        for(int i=ind;i < min(n, ind + k) ;i++){
+            curr=max(arr[i],curr);
+            length++;
+            int ans=length*curr+helper(i+1,arr,k,vec);
+            maxi=max(maxi,ans);
+            
+            
+            
+        }
+        return vec[ind]=maxi;
+    }
+};
