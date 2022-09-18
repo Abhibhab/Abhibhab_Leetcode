ff6ec0f47eee93a74429c27810060c212151441a
Abhibhab Mitra Thakur <70086453+Abhibhab@users.noreply.github.com>
2022-09-18 19:23:04 +0530

    Time: 69 ms (22.18%), Space: 30.8 MB (93.06%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
@@ -11,20 +11,73 @@
  */
 class Solution {
 public:
-    int count=0;
+    // int count=0;
     int countNodes(TreeNode* root) {
         if(root==NULL)return 0;
         int left=0,right=0;
-        if(root->left){
-            left=countNodes(root->left);
+       left=heightl(root);
+       right=heightr(root);
+        if(left==right)return (1<<left)-1;
+        return 1+countNodes(root->right)+countNodes(root->left);
+
+        
+//         if(root->left){
+//             left=countNodes(root->left);
             
-        }
-        if(root->right){
-            right=countNodes(root->right);
+//         }
+//         if(root->right){
+//             right=countNodes(root->right);
             
-        }
-        return left+right+1;
+//         }
+//         return left+right+1;
         
         
     }
+    int heightr(TreeNode* root){
+        int count=0;
+        while(root){
+            count++;
+            root=root->right;
+        }
+        return count;
+        
+    }
+     int heightl(TreeNode* root){
+        int count=0;
+        while(root){
+            count++;
+            root=root->left;
+        }
+        return count;
+        
+    }
+//     int findHeightLeft(TreeNode* cur) {
+//         int hght = 0; 
+//         while(cur) {
+//             hght++; 
+//             cur = cur->left; 
+//         }
+//         return hght; 
+//     }
+//     int findHeightRight(TreeNode* cur) {
+//         int hght = 0; 
+//         while(cur) {
+//             hght++; 
+//             cur = cur->right; 
+//         }
+//         return hght; 
+//     }
+//     int countNodes(TreeNode* root) {
+//         if(root == NULL) return 0; 
+        
+//         int lh = findHeightLeft(root); 
+//         int rh = findHeightRight(root); 
+        
+//         if(lh == rh) return (1<<lh) - 1; 
+        
+//         int leftNodes = countNodes(root->left);
+//         int rightNodes = countNodes(root->right);
+        
+//         return 1 + leftNodes + rightNodes; 
+//     }
 };
