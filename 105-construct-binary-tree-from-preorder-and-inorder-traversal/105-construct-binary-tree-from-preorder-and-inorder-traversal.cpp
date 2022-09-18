28085bf2b085c8850ee25f513aa6314bd1ee58ea
Abhibhab Mitra Thakur <70086453+Abhibhab@users.noreply.github.com>
2022-09-19 00:28:12 +0530

    Time: 65 ms (23.77%), Space: 26 MB (79.78%) - LeetHub

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTreeHelper(vector<int>& in, vector<int>& pre, int inS, int inE, int preS, int preE) {
+	if (inS > inE) {
+		return NULL;
+	}
+
+	int rootData = pre[preS];
+	int rootIndex = -1;
+	for (int i = inS; i <= inE; i++) {
+		if (in[i] == rootData) {
+			rootIndex = i;
+			break;
+		}
+	}
+
+	int lInS = inS; //left inorder start
+	int lInE = rootIndex - 1;//left inorder end
+	int lPreS = preS + 1;//left preorder start
+	int lPreE = lInE - lInS + lPreS;//left preorder end
+	int rPreS = lPreE + 1;//right preorder start
+	int rPreE = preE;//right preorder end
+	int rInS = rootIndex + 1;//right inorder start
+	int rInE = inE;//right inorder end
+	TreeNode*  root = new TreeNode(rootData);
+	root->left = buildTreeHelper(in, pre, lInS, lInE, lPreS, lPreE);
+	root->right = buildTreeHelper(in, pre, rInS, rInE, rPreS, rPreE);
+	return root;
+}
+ 
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        return buildTreeHelper(inorder,preorder,0,inorder.size()-1,0,preorder.size()-1);
+        
+    }
+};
