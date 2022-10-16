d0c1c7708b4c0dd79e3be77c3114a119c4aff776
Abhibhab Mitra Thakur <70086453+Abhibhab@users.noreply.github.com>
2022-10-16 13:22:25 +0530

    Time: 96 ms (47.66%), Space: 7.9 MB (30.04%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
-    int minDifficulty(vector<int>& A, int D) {
-        int n = A.size(), inf = 1e9, maxd;
-        if (n < D) return -1;
-        vector<int> dp(n + 1, 1e9);
-        dp[n] = 0;
-        for (int d = 1; d <= D; ++d) {
-            for (int i = 0; i <= n - d; ++i) {
-                maxd = 0, dp[i] = inf;
-                for (int j = i; j <= n - d; ++j) {
-                    maxd = max(maxd, A[j]);
-                    dp[i] = min(dp[i], maxd + dp[j + 1]);
-                }
-            }
+    int helper(int i,int d,vector<int>&dif,vector<vector<int>>&dp){
+        int size=dif.size();
+        if(i==size && d==0)return 0;
+        if(i==size||d==0)return 1e9;
+        if(dp[i][d]!=-1)return dp[i][d];
+        int ans=INT_MAX;
+        int maxi=INT_MIN;
+        for(int j=i;j<size;j++){
+            maxi=max(maxi,dif[j]);
+            ans=min(ans,maxi+helper(j+1,d-1,dif,dp));
+            
         }
-        return dp[0];
+        return dp[i][d]=ans;
+        
+        
+    }
+    int minDifficulty(vector<int>& jD, int d) {
+        int n=jD.size();
+        if(n<d)      
+            return -1;
+        vector<vector<int>>dp(n+1,vector<int>(d+1,-1));
+        return helper(0,d,jD,dp);
+        
     }
 };
