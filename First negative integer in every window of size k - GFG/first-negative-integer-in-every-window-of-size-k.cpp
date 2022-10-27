ed6a53a090889adf87f2c2b83c3d7fb8e3b34c60
Abhibhab Mitra Thakur <70086453+Abhibhab@users.noreply.github.com>
2022-10-27 19:53:22 +0530

    Added solution - LeetHub

--- First negative integer in every window of size k - GFG/first-negative-integer-in-every-window-of-size-k.cpp
+++ First negative integer in every window of size k - GFG/first-negative-integer-in-every-window-of-size-k.cpp
@@ -28,33 +28,66 @@ int main() {
 
 // } Driver Code Ends
 
-
-vector<long long> printFirstNegativeInteger(long long int A[],
-                                             long long int N, long long int K) {
+// void showlist(list<long long> g)
+// {
+//     list<long long>::iterator it;
+//     for (it = g.begin(); it != g.end(); ++it)
+//         cout << '\t' << *it;
+//     cout << '\n';
+// }
+vector<long long> printFirstNegativeInteger(long long int arr[],
+                                             long long int n, long long int k) {
                                                  
                                                  
                                                  
-    long long i=0;
- long long j=0;
- vector<long long> ans;
- list<long long> l;
-  while (j<N){
-    if (A[j]<0) 
-      l.push_back(A[j]);
-    if (j-i+1<K) j++;
-    else if ((j-i+1)==K)
-    {
-      if (l.size()==0)
-        ans.push_back(0);
-      else
-        ans.push_back(l.front());
-      if(A[i]<0)
-        l.pop_front();
-      i++;
-      j++;
+    vector<long long>ans;
+    list<long long>list;
+    long long j=0,i=0;
+    while(j<n){
+        if(arr[j]<0){
+            list.push_back(arr[j]);
+        }
+        if(j-i+1<k){
+            j++;
+        }
+        else if((j-i+1)==k){
+            if(list.size()==0){
+                ans.push_back(0);
+            }else{
+                ans.push_back(list.front());
+            }
+             if(arr[i]<0){
+                list.pop_front();
+            }
+            // showlist(list);
+            // cout<<endl;
+            
+           i++;
+           j++;
+           
+        }
+        
+        
     }
-  }
-  return ans;
+    return ans;
+    
+//      while (j<N){
+//     if (A[j]<0) 
+//       l.push_back(A[j]);
+//     if (j-i+1<K) j++;
+//     else if ((j-i+1)==K)
+//     {
+//       if (l.size()==0)
+//         ans.push_back(0);
+//       else
+//         ans.push_back(l.front());
+//       if(A[i]<0)
+//         l.pop_front();
+//       i++;
+//       j++;
+//     }
+//   }
+//   return ans;
                                                  
                                                 
                                                  
