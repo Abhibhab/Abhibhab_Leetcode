648820741160331710dfedac572f65556ea89aac
Abhibhab Mitra Thakur <70086453+Abhibhab@users.noreply.github.com>
2023-07-07 19:08:48 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,39 +3,69 @@
 using namespace std;
 
 // } Driver Code Ends
+
+class DSU{
+    public:
+    vector<int>size,par;
+    DSU(int n){
+        size.resize(n+1,1);
+        par.resize(n+1);
+        for(int i=0;i<=n;i++){
+            par[i]=i;
+        }
+        
+    }
+    int findpar(int node) {
+        if (node == par[node])
+            return node;
+        return par[node] = findpar(par[node]);
+    }
+    void unisize(int x,int y){
+        int u=findpar(x);
+        int v=findpar(y);
+        if(u==v)return;
+        if(size[u]>size[v]){
+            par[v]=u;
+            size[v]+=size[u];
+        }else{
+            par[v]=u;
+            size[u]+=size[v];
+        }
+    }
+};
+
 class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+	
     int spanningTree(int V, vector<vector<int>> adj[])
     {
-        // code here
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        pq.push({0,0});
-        vector<int>vis(V,0);
-        int ans=0;
-        while(pq.size()){
-            auto ele=pq.top();
-            pq.pop();
-            int node=ele.second;
-            int weight=ele.first;
-            if(vis[node]==1)continue;
-            vis[node]=1;
-            ans+=weight;
-            
-            for(auto it : adj[node]){
-                int adjnode=it[0];
-                int paisa=it[1];
-                if(!vis[adjnode]){
-                    pq.push({paisa,adjnode});
-                    
-                }
-                
-                
-                
-            }
-        }
-        return ans;
+     vector<pair<int,pair<int,int>>>edges;
+     for(int i=0;i<V;i++){
+         for(auto it:adj[i]){
+         int node=i;
+         int nodeto=it[0];
+         int weight=it[1];
+         edges.push_back({weight,{node,nodeto}});
+         }
+     }
+     DSU ds(V);
+     sort(edges.begin(),edges.end());
+     int ans=0;
+     for(auto it:edges){
+         int weight=it.first;
+         int node=it.second.first;
+         int nodeto=it.second.second;
+         if(ds.findpar(nodeto)!=ds.findpar(node)){
+             ans+=weight;
+             ds.unisize(node,nodeto);
+         }
+     }
+     return ans;
+     
+        
+        
         
     }
 };
