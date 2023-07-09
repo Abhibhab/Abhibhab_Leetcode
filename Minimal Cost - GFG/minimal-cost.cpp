2f6bc25188c98888fdcaa9807d861f4654ca0f48
Abhibhab Mitra Thakur <70086453+Abhibhab@users.noreply.github.com>
2023-07-09 22:11:41 +0530

    Added solution - LeetHub

--- Minimal Cost - GFG/minimal-cost.cpp
+++ Minimal Cost - GFG/minimal-cost.cpp
@@ -18,10 +18,24 @@ class Solution {
         }
         return dp[ind]= ans;
     }
-    int minimizeCost(vector<int>& height, int n, int k) {
+    int minimizeCost(vector<int>& heights, int n, int k) {
         // Code here
-        vector<int>dp(n,-1);
-        return helper(n-1,k,height,dp);
+        // vector<int>dp(n,-1);
+        // return helper(n-1,k,height,dp);
+        //TABULAR
+        vector<int>dp(n,0);
+        for(int ind=1;ind<n;ind++){
+        int ans=INT_MAX;
+        for(int i=1;i<=k;i++){
+            if(ind-i>=0){
+                ans=min(ans,dp[ind-i]+abs(heights[ind]-heights[ind-i]));
+            }
+            
+        }
+       dp[ind]= ans;
+            
+        }
+        return dp[n-1];
     }
 };
 
